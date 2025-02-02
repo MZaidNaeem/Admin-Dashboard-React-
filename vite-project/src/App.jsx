eb9df3805f5f24fcc7d2616141f6dae2a70cb4e1
MZaidNaeem <officialcodexyz@gmail.com>
2025-02-02 09:19:39 -0800

    context api added in the app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 
-import React from "react";
-// import './App.css'
+import React, { createContext } from "react";
+import './App.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppLayout } from './Layout/AppLayout';
@@ -8,8 +8,12 @@ import ErrorPage from './Layout/ErrorPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Home } from './Pages/Home';
 
+export const AppContext = createContext();
 
 function App() {
+
+  const stock = 55;
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -29,9 +33,11 @@ function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}></RouterProvider>
-    </QueryClientProvider>
+    <AppContext.Provider value={{ stock }}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
+    </AppContext.Provider>
   )
 
 
